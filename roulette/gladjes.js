aa32c8283e8335935c0454838f727f70d409a2ee
warp420 <46268192+warp420@users.noreply.github.com>
2019-01-24 20:46:52 +0100

    Update gladjes.js

--- roulette/gladjes.js
+++ roulette/gladjes.js
@@ -1,31 +1,28 @@
 var quotes = [
-    'Heb jij je belastingen al gedaan?
-
-Ik ben namelijk ook aftrekbaar.',
+    'Heb jij je belastingen al gedaan? Ik ben namelijk ook aftrekbaar.',
     'Is je vader DJ? Want jij bent echt een plaatje!',
     'Ben jij een hond? Want ik zou je urenlang anaal nemen.',
     'Doet je vader aan incest? Want als ik je vader was zou ik aan incest doen!',
     'Ik heb een goed-betaalde baan, hallo.',
     'Wil je me alsjeblieft even aaien?',
-    'Een goedendag wonderschone dame, wellicht zint het u om met mij hulpmotoren te gaan bewonderen ?
-*tipt agrarisch hoofddeksel',
-    'Paal Onderverhaal NVVW in een kanaal naar keuze.',
-    'Paal een huilende kan in een kanaal naar keuze.',
-    'Maak een ASMR opname.',
-    'Forceer een dode meem.',
-    'Forceer een dode meem.',
-    'Forceer een dode meem.',
-    'Schijtpaal in #echte_praat.',
-    'Je bent aangesteld als stadt!',
-    'Paal een angelsakse meem in #lekker_trekken',
-    'Paal een stokbroodsakse meem in #lekker_trekken',
-    'Paal een MijnBouw NVVW in het NVVW kanaal.',
-   'Paal een Avonturen Tijd NVVW in het NVVW kanaal.',
-    'PM een willekeurig persoon met een schijtpaal.',
-    'PM een willekeurig persoon met een NVVW.',
-    'PM een willekeurig stadt met een liefdesverklaring.',
-    'Reageer :gelezen: onder het laatst gestuurde bericht in #lekker_trekken.',
-    'Begin een verhaal in #echte_praat, met als onderwerp dat je hond homo zou zijn.'
+    'Een goedendag wonderschone dame, wellicht zint het u om met mij hulpmotoren te gaan bewonderen? *tipt agrarisch hoofddeksel',
+    'Hee! Ehm... ja dus uhhh... haha mooi weer hè? Kut... hee houdoei... ik bedoel d... laat maar.',
+    'Als jij een kraakpand was, had ik je helemaal uitgewoond.',
+    'Kom je uit Tsjernobyl, want je ziet er stralend uit.',
+    'Hoe zwaar is een ijsbeer? Zwaargenoeg om het ijs te breken.',
+    'Stel je voor, er zijn 3 boten in nood op zee. 1 boot zit vol met hoogzwangere paarden. 2e boot zit vol met acrobatische panda\'s. En de laatste zit vol met eenhoorns. Stel je voor, jij was een reddingshelikopter. Wat zou jouw reddingstelefoonnummer dan zijn?',
+    'UwU *haalt  vlees scepter uit broek*',
+    'Zal ik je even beffen om het ijs te breken?',
+    'Vind jij deze lap ook zo naar chloroform ruiken?',
+    'Hoi, ik heet Bert. Hoe heet ben jij? (Exclusief voor mensen die Bert heten)',
+    'Hoi, ik hoop stiekem dat jouw eisen lager zijn dan de mijne...',
+    'Is het de of het magazijn? [antwoord] Nee, jij mag er zijn!',
+   'Als jij een frikandel was, was jij een frikandel speciaal.',
+    'Ben jij een vogel? "Nee" Waarom zit je dan vanavond op een harde stok met zaad in je bek.',
+    'Is jouw vader toevallig timmerman? "Nee" Waarom heb je dan zo\'n prachtige voorgevel?',
+    'Is jouw vader houthakker? Want je hebt een enorme bos hout voor de deur.',
+    'Ik hoop dat je een aluminium afgietsel hebt gemaakt van je baarmoeder want ik <br> kom zo hard dat ik door je ruggegraat schiet.Ik bedoel, hoi, ik heet Bert',
+    'Je hoeft geen Twents te kunnen, als je maar plat kan.
     
     ];
 
